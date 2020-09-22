8830470977e640237f548f87730bfb74e3efe979
German <gd.martinez@uniandes.edu.co>
2020-09-22 17:07:25 -0500

    Faltó lo de las tarjetas :(

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const dataDir = "data.json";
 
-function borrarFila(indexRow) {
-  let nodo = document.getElementById(`${indexRow}`);
-  if (nodo.parentNode) {
-    nodo.parentNode.removeChild(nodo);
+function borrarFila(row2Delete) {
+  // Otra forma de hacerlo
+  //let nodo = document.getElementById(`${row2Delete}`);
+  //if (nodo.parentNode) {
+  //  nodo.parentNode.removeChild(nodo);
+  //}
+  let i = row2Delete.parentNode.parentNode.rowIndex;
+  document.getElementById("punto1").deleteRow(i - 1);
+}
+
+function editarFila(row2Edit) {
+  let i = row2Edit.parentNode.parentNode.rowIndex;
+  row = document.getElementById("punto1").rows[i - 1];
+  for (i = 0; i < 3; i++) {
+    row.cells[i].contentEditable = "true";
   }
 }
 
@@ -11,7 +22,7 @@ function punto1(dataList) {
   let table1 = document.getElementById("punto1");
   dataList.forEach((element, index) => {
     let row = document.createElement("tr");
-    row.setAttribute("id", `${index}`);
+    //row.setAttribute("id", `${index}`);
 
     let lastName = document.createElement("td");
     let node1 = document.createTextNode(`${element.last_lane}`);
@@ -33,10 +44,20 @@ function punto1(dataList) {
     let deleteRow = document.createElement("td");
     let btn = document.createElement("button");
     btn.className = "btn btn-danger";
+    btn.innerHTML = "Eliminar fila";
     btn.onclick = function () {
-      borrarFila(index);
+      borrarFila(this);
     };
+
+    let btn2 = document.createElement("button");
+    btn2.className = "btn btn-primary";
+    btn2.innerHTML = "Editar fila";
+    btn2.onclick = function () {
+      editarFila(this);
+    };
+
     deleteRow.appendChild(btn);
+    deleteRow.appendChild(btn2);
 
     row.appendChild(lastName);
     row.appendChild(firstName);
@@ -120,7 +141,7 @@ function punto2(dataList) {
     });
 }
 
-function punto5() {
+function punto7() {
   let boton = document.getElementById("deleteAll");
   boton.addEventListener("click", function alertar() {
     let table1 = document.getElementById("punto1");
@@ -132,16 +153,42 @@ function punto5() {
   modalBorrar.setAttribute("data-dismiss", "modal");
 }
 
-function punto7() {
-  let boton = document.getElementById("deleteAll");
-  boton.addEventListener("click", function alertar() {
-    let table1 = document.getElementById("punto1");
-    while (table1.firstChild) {
-      table1.removeChild(table1.firstChild);
-    }
-  });
-  let modalBorrar = document.getElementById("modalBorrar");
-  modalBorrar.setAttribute("data-dismiss", "modal");
+function addRow() {
+  let table = document.getElementById("punto1");
+  var row = table.insertRow(-1);
+  var ln = row.insertCell(0);
+  var fn = row.insertCell(1);
+  var email = row.insertCell(2);
+
+  var photo = row.insertCell(3);
+  var img = document.createElement("img");
+  img.src = document.getElementById("photo").value;
+  photo.appendChild(img);
+
+  var but = row.insertCell(4);
+
+  var btn = document.createElement("btn");
+  btn.className = "btn btn-danger";
+  btn.innerHTML = "Eliminar fila";
+  btn.onclick = function () {
+    borrarFila(this);
+  };
+
+  var btn2 = document.createElement("btn");
+  btn2.className = "btn btn-primary";
+  btn2.innerHTML = "Editar fila";
+  btn2.onclick = function () {
+    editarFila(this);
+  };
+
+  but.appendChild(btn);
+  but.appendChild(btn2);
+
+  ln.innerHTML = document.getElementById("lastName").value;
+  fn.innerHTML = document.getElementById("firstName").value;
+  email.innerHTML = document.getElementById("email").value;
+
+  return false;
 }
 
 fetch(dataDir)
